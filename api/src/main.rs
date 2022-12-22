fdc4b6c6d4d62e4ac68edb46e50d42592f47d6c7
squak <squakrazv@gmail.com>
2022-12-22 03:30:23 +0300

    feat: add cors support

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,8 +6,10 @@ use fuzzy_cognitive_model::errors::QueryError;
 use fuzzy_cognitive_model::models::User;
 use fuzzy_cognitive_model::services;
 use rocket::serde::json::Json;
+use rocket_cors::AllowedOrigins;
 use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 use rocket_okapi::{openapi, openapi_get_routes};
+use std::env;
 
 /// Get an user by id
 #[openapi(tag = "users")]
@@ -28,7 +30,19 @@ fn get_docs() -> SwaggerUIConfig {
 #[launch]
 fn rocket() -> _ {
     dotenv().unwrap();
+
+    let allowed_origins = AllowedOrigins::some_exact(
+        &serde_json::from_str::<Box<[String]>>(&env::var("CORS_ALLOWED_ORIGINS").unwrap()).unwrap(),
+    );
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
     rocket::build()
         .mount("/api/v1", openapi_get_routes![user])
         .mount("/api/v1/docs", make_swagger_ui(&get_docs()))
+        .attach(cors)
 }
