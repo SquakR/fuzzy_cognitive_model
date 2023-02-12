33ad569ce0391a97e6ce25ef1185e93fb733e1db
squak <squakrazv@gmail.com>
2023-02-13 02:32:38 +0300

    feat: add mailing service

--- api/src/services.rs
+++ api/src/services.rs
@@ -1,2 +1,3 @@
+pub mod mailing_services;
 pub mod session_services;
 pub mod users_services;
---
+++ api/src/services/mailing_services.rs
@@ -0,0 +1,39 @@
+use crate::{errors::AppError, utils};
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor};
+
+pub async fn send_message(to_email: &str, subject: &str, body: &str) -> Result<(), AppError> {
+    let smtp_host = utils::get_env("SMTP_HOST");
+    let smtp_login = utils::get_env("SMTP_LOGIN");
+    let smtp_password = utils::get_env("SMTP_PASSWORD");
+
+    let smtp_credentials = Credentials::new(smtp_login.clone(), smtp_password);
+    let mailer_builder = match AsyncSmtpTransport::<Tokio1Executor>::relay(smtp_host.as_str()) {
+        Ok(builder) => builder,
+        Err(_) => return Err(AppError::InternalServerError),
+    };
+    let mailer = mailer_builder.credentials(smtp_credentials).build();
+    let send_result = send_email_smtp(&mailer, smtp_login.as_str(), to_email, subject, body).await;
+    if send_result.is_err() {
+        return Err(AppError::InternalServerError);
+    }
+    Ok(())
+}
+
+async fn send_email_smtp(
+    mailer: &AsyncSmtpTransport<Tokio1Executor>,
+    from: &str,
+    to: &str,
+    subject: &str,
+    body: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let email = Message::builder()
+        .from(from.parse()?)
+        .to(to.parse()?)
+        .subject(subject)
+        .body(body.to_string())?;
+
+    mailer.send(email).await?;
+
+    Ok(())
+}
